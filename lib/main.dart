4cf53e8b413ae87e79ba590c88c3cff3ade41817
aryanmaheshwari1420 <98485902+aryanmaheshwari1420@users.noreply.github.com>
2022-08-14 01:07:28 +0530

    new folder and dependecny adding

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 // @dart=2.9
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -6,18 +5,18 @@ import 'package:movierecommend/Home.dart';
 import 'package:movierecommend/Login.dart';
 import 'package:movierecommend/SignUp.dart';
 import 'package:movierecommend/WelcomeScreen.dart';
-
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    theme: ThemeData(brightness: Brightness.light,
-    primaryColor: Colors.green
-    ),
+    theme: ThemeData(brightness: Brightness.light, primaryColor: Colors.green),
     home: WelcomeScreen(),
     routes: {
       'S1': (context) => WelcomeScreen(),
       'S2': (context) => Login(),
-      'S3': (context) =>  SignUp(),
+      'S3': (context) => SignUp(),
       'S4': (context) => Home()
     },
   ));
